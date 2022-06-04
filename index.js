cd6af6c6ca68d86cbf79098dc7d80055889df2d8
Marco <marcovelardejr@gmail.com>
2022-06-04 13:32:09 -0500

    commit tree da858307854deaacbd8f154bf8ee0f8e76e82b53

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
 /* 
-  Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
+
 */
 
 /*
@@ -12,14 +25,33 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("hello"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("javascript"));
 }
 
 module.exports = isPalindrome;
